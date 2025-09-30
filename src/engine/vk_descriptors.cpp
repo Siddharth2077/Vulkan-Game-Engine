171c586320b4777eaeb5e20f40d20a727b6f1ea4
Siddharth2077 <siddharth.muddu.work2002@gmail.com>
2025-09-30 17:26:50 +0530

    descriptor-set layout and allocator structs

--- src/engine/vk_descriptors.cpp
+++ src/engine/vk_descriptors.cpp
@@ -46,19 +46,61 @@ VkDescriptorSetLayout DescriptorLayoutBuilder::build(VkDevice device, VkShaderSt
 // Descriptor Set Allocator: Method Definitions
 
 void DescriptorSetAllocator::init_descriptor_pool(VkDevice device, uint32_t max_descriptor_sets, std::span<PoolSizeRatio> pool_size_ratios) {
+    // Create the descriptor pool sizes, based on the ratios and max_descriptor_sets value provided:
+    std::vector<VkDescriptorPoolSize> descriptor_pool_sizes{};
+    for (PoolSizeRatio pool_size : pool_size_ratios) {
+        descriptor_pool_sizes.push_back(
+            VkDescriptorPoolSize{
+                .type = pool_size.descriptor_type,
+                .descriptorCount = static_cast<uint32_t>(max_descriptor_sets * pool_size.ratio)
+            }
+        );
+    }
 
+    // Create the descriptor-pool:
+    VkDescriptorPoolCreateInfo pool_create_info{};
+    pool_create_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    pool_create_info.pNext = nullptr;
+    pool_create_info.flags = 0;
+    pool_create_info.maxSets = max_descriptor_sets;
+    pool_create_info.poolSizeCount = static_cast<uint32_t>(descriptor_pool_sizes.size());
+    pool_create_info.pPoolSizes = descriptor_pool_sizes.data();
+
+    // Create the descriptor-pool and store it in the struct
+    VkResult result = vkCreateDescriptorPool(device, &pool_create_info, nullptr, &descriptor_pool);
+    if (result != VK_SUCCESS) {
+        VK_LOG_ERROR("Failed to create descriptor pool!");
+        throw std::runtime_error("Failed to create descriptor pool!");
+    }
+    VK_LOG_SUCCESS("Created descriptor pool");
 }
 
 void DescriptorSetAllocator::clear_all_descriptor_sets(VkDevice device) {
-
+    // Resetting the descriptor-pool destroys all the descriptor-sets creates from it.
+    vkResetDescriptorPool(device, descriptor_pool, 0);
 }
 
 void DescriptorSetAllocator::destroy_descriptor_pool(VkDevice device) {
-
+    vkDestroyDescriptorPool(device, descriptor_pool, nullptr);
 }
 
 VkDescriptorSet DescriptorSetAllocator::allocate_descriptor_set(VkDevice device, VkDescriptorSetLayout descriptor_set_layout) {
+    VkDescriptorSetAllocateInfo allocate_info{};
+    allocate_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocate_info.pNext = nullptr;
+    allocate_info.descriptorPool = descriptor_pool;
+    allocate_info.descriptorSetCount = 1;
+    allocate_info.pSetLayouts = &descriptor_set_layout;
 
+    VkDescriptorSet descriptor_set {VK_NULL_HANDLE};
+    VkResult result = vkAllocateDescriptorSets(device, &allocate_info, nullptr);
+    if (result != VK_SUCCESS) {
+        VK_LOG_ERROR("Failed to allocate descriptor set!");
+        throw std::runtime_error("Failed to allocate descriptor set!");
+    }
+    VK_LOG_SUCCESS("Created descriptor set");
+
+    return descriptor_set;
 }
 
 
--- src/engine/vk_descriptors.h
+++ src/engine/vk_descriptors.h
@@ -15,11 +15,15 @@ public:
 
 struct DescriptorSetAllocator {
 private:
+    /// @brief Describe the @code ratio@endcode of each type of Descriptor to allocate from the Pool.
+    /// @note Suppose @code max_descriptor_sets@endcode = 100, and @code ratio@endcode = 0.5 for a given @code descriptor_type@endcode, 50 such descriptors will be allocated in the Pool
+    /// @attention This ratio is against the @code max_descriptor_sets@endcode value passed during @code init_descriptor_pool()@endcode
     struct PoolSizeRatio {
         VkDescriptorType descriptor_type;
         float ratio;
     };
-    VkDescriptorPool descriptor_pool;
+
+    VkDescriptorPool descriptor_pool {VK_NULL_HANDLE};
 
 public:
     void init_descriptor_pool(VkDevice device, uint32_t max_descriptor_sets, std::span<PoolSizeRatio> pool_size_ratios);
